fdc2dd7e4e8851c3660bfd7d5893e12f33649f25
zdzarsky <wojtek.zdz@gmail.com>
2017-05-22 22:31:23 +0200

    Final Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,9 +1,12 @@
 package com.company;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("Witamy w systemie totalizatora!");
+        TimeUnit.SECONDS.sleep(1);
         GameSystem sys = new GameSystem();
         User windows = new WindowsUser();
         User ios = new iOsUser();
@@ -18,14 +21,36 @@ public class Main {
         sys.registerObserver(black);
         sys.registerObserver(and);
         Game g = new GameType1();
-        System.out.println("Utworzono grę GameType1");
-        sys.playGame(g);
         sys.registerObserverToGame(ios, g);
         sys.registerObserverToGame(windows, g);
         sys.registerObserverToGame(black, g);
         sys.registerObserverToGame(and, g);
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Utworzono grę GameType1");
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Nowa gra rozpoczyna się za: ");
+        for(int i = 3; i >=0; i--){
+            TimeUnit.SECONDS.sleep(1);
+            System.out.print("\r" + i);
+        }
+        System.out.println();
         System.out.println("System wykonał losowanie w grze GameType1");
+        sys.playGame(g);
+
         sys.notifyEveryone();
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Wyrzucam obserwatora : " + ios);
+        sys.kickObserver(ios);
+        TimeUnit.SECONDS.sleep(1);
+        System.out.println("Nowa gra rozpoczyna się za: ");
+        for(int i = 3; i >=0; i--){
+            TimeUnit.SECONDS.sleep(1);
+            System.out.print("\r" + i);
+        }
+        System.out.println();
+        sys.playGame(g);
+        sys.notifyEveryone();
+
 
 
 
--- src/com/company/User.java
+++ src/com/company/User.java
@@ -6,6 +6,11 @@ import java.util.Random;
 public abstract class User {
     private String id = "";
 
+    @Override
+    public String toString(){
+        return this.id;
+    }
+
     public void initialize() {
         Random r = new Random();
         for (int i = 0; i < 6; i++) {
@@ -17,6 +22,6 @@ public abstract class User {
     }
 
     public void sendResults(Score score) {
-        System.out.println("Użytkownik " + id + " otrzymał wynik " + score);
+        System.out.print("Użytkownik " + id + " otrzymał wynik " + score);
     }
 }
